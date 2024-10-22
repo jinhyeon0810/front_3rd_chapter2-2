5fe9f3d22278ffd8d115dd23338142140900f50b
김진현 <jinhyeon0810@gmail.com>
2024-10-23 02:15:05 +0900

    refactor CartPage 쿠폰적용부분 컴포넌트화

--- src/refactoring/pages/CartPage.tsx
+++ src/refactoring/pages/CartPage.tsx
@@ -1,5 +1,6 @@
 import { Coupon, Product } from "../../types.ts";
 import CartItemDetails from "../components/cart/CartItemDetails.tsx";
+import CouponApply from "../components/cart/CouponApply.tsx";
 import ProductList from "../components/cart/ProductList.tsx";
 import { useCart } from "../hooks/index.ts";
 import { getMaxApplicableDiscount } from "../hooks/utils/cartUtils.ts";
@@ -66,32 +67,11 @@ export const CartPage = ({ products, coupons }: Props) => {
             })}
           </div>
 
-          <div className="mt-6 bg-white p-4 rounded shadow">
-            <h2 className="text-2xl font-semibold mb-2">쿠폰 적용</h2>
-            <select
-              onChange={(e) => applyCoupon(coupons[parseInt(e.target.value)])}
-              className="w-full p-2 border rounded mb-2"
-            >
-              <option value="">쿠폰 선택</option>
-              {coupons.map((coupon, index) => (
-                <option key={coupon.code} value={index}>
-                  {coupon.name} -{" "}
-                  {coupon.discountType === "amount"
-                    ? `${coupon.discountValue}원`
-                    : `${coupon.discountValue}%`}
-                </option>
-              ))}
-            </select>
-            {selectedCoupon && (
-              <p className="text-green-600">
-                적용된 쿠폰: {selectedCoupon.name}(
-                {selectedCoupon.discountType === "amount"
-                  ? `${selectedCoupon.discountValue}원`
-                  : `${selectedCoupon.discountValue}%`}{" "}
-                할인)
-              </p>
-            )}
-          </div>
+          <CouponApply
+            coupons={coupons}
+            applyCoupon={applyCoupon}
+            selectedCoupon={selectedCoupon}
+          />
 
           <div className="mt-6 bg-white p-4 rounded shadow">
             <h2 className="text-2xl font-semibold mb-2">주문 요약</h2>
