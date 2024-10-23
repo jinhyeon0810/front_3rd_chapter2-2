5148d88abe3384c1bacc321884e274c7e6b0ea9d
김진현 <jinhyeon0810@gmail.com>
2024-10-23 16:10:35 +0900

    refactor 관리자페이지 상품추가 섹션 데이터,UI 로직 useHook으로 구분

--- src/refactoring/components/admin/product/NewProductForm.tsx
+++ src/refactoring/components/admin/product/NewProductForm.tsx
@@ -1,28 +1,19 @@
-import { useState } from "react";
 import { Product } from "../../../../types";
+import { useNewProduct } from "../../../hooks/admin/useNewProduct";
 
 interface NewProductFormProps {
   onProductAdd: (product: Product) => void;
 }
 
-const initProductState = {
-  name: "",
-  price: 0,
-  stock: 0,
-  discounts: [],
-};
-
 const NewProductForm = ({ onProductAdd }: NewProductFormProps) => {
-  const [showNewProductForm, setShowNewProductForm] = useState(false);
-  const [newProduct, setNewProduct] =
-    useState<Omit<Product, "id">>(initProductState);
+  const {
+    showNewProductForm,
+    setShowNewProductForm,
+    newProduct,
+    setNewProduct,
+    handleAddNewProduct,
+  } = useNewProduct(onProductAdd);
 
-  const handleAddNewProduct = () => {
-    const productWithId = { ...newProduct, id: Date.now().toString() };
-    onProductAdd(productWithId);
-    setNewProduct(initProductState);
-    setShowNewProductForm(false);
-  };
   return (
     <>
       <button
---
+++ src/refactoring/hooks/admin/useNewProduct.ts
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { Product } from "../../../types";
+
+const initProductState = {
+  name: "",
+  price: 0,
+  stock: 0,
+  discounts: [],
+};
+export const useNewProduct = (onProductAdd: (product: Product) => void) => {
+  const [showNewProductForm, setShowNewProductForm] = useState(false);
+  const [newProduct, setNewProduct] =
+    useState<Omit<Product, "id">>(initProductState);
+
+  const handleAddNewProduct = () => {
+    const productWithId = { ...newProduct, id: Date.now().toString() };
+    onProductAdd(productWithId);
+    setNewProduct(initProductState);
+    setShowNewProductForm(false);
+  };
+
+  return {
+    showNewProductForm,
+    setShowNewProductForm,
+    newProduct,
+    setNewProduct,
+    handleAddNewProduct,
+  };
+};
